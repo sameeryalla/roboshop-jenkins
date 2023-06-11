3fbcadc8018e7441a1560df9c8cf8eba3be97ed1
sameeryalla <sameeryalla@gmail.com>
2023-06-11 09:00:42 +0530

    ansiColor added to groovy file

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -5,6 +5,9 @@ def call() {
                 label 'workstation'
             }
         }
+        options {
+            ansiColor('xterm')
+        }
         parameters {
             choice(name: 'env', choices: ['dev','prod'], description: 'pick Environment')
         }
