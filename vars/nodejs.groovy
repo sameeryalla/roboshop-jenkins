27de8e3de5e3c546c4c0cc2a17f6b28fb677a092
sameeryalla <sameeryalla@gmail.com>
2023-06-12 21:23:48 +0530

    sonarqube wait introduced

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -13,7 +13,8 @@ def call() {
         stages {
             stage ('Code Quality Check'){
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.157:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                    sh 'ls -l'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.157:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
             stage ('Unit Test Cases'){
