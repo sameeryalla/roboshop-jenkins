94e55c284109a61c32530f6df78d37ae7f46b8ff
sameeryalla <sameeryalla@gmail.com>
2023-06-12 21:33:25 +0530

    shipping jenkinsfile with maven setup

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -13,12 +13,13 @@ def call() {
         stages {
             stage ('Code Compile'){
                 steps {
-                    sh 'echo Code Compile'
+                    sh 'mvn compile'
                 }
             }
             stage ('Code Quality Check'){
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component}'
+                    sh 'ls -l'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.91.157:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
             stage ('Unit Test Cases'){
